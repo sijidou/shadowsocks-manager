3defead33bbcca9f86754b11a973a359ebbaad5f
sijidou <16715966+sijidou@users.noreply.github.com>
2024-10-23 15:54:26 +0800

    Update runShadowsocks.js

--- init/runShadowsocks.js
+++ init/runShadowsocks.js
@@ -30,7 +30,7 @@ const run = async () => {
     shadowsocks = spawn('ssserver', ['-m', method, '-p', '65535', '-k', tempPassword, '--manager-address', config.shadowsocks.address]);
   } else if(runParams.includes('rust')) {
     type = 'rust';
-    shadowsocks = spawn('ssmanager', [ '-m', method, '-U', '--manager-address', config.shadowsocks.address, ...pluginOptions]);
+    shadowsocks = spawn('ssmanager', [ '-m', method, '-U', '--tcp-no-delay', '--tcp-fast-open', '--tcp-multipath', '-n', '51200', '--manager-address', config.shadowsocks.address, ...pluginOptions]);
   } else {
     shadowsocks = spawn('ss-manager', [ '-v', '-m', method, '-u', '--manager-address', config.shadowsocks.address, ...pluginOptions]);
   }
